3cb56ad3dbcdc7abe481666a4c23e891262bb70b
Nazmul Alam <nazmuldipu@gmail.com>
2018-09-01 17:32:36 +0600

    index page done

---
+++ src/home/components/bottom/bottom.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bottom',
+  templateUrl: './bottom.component.html',
+  styleUrls: ['./bottom.component.scss']
+})
+export class BottomComponent {}
