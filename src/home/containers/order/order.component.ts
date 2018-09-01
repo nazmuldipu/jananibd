04b597e1df9cf69ce901d6eec1dd83cf32c4fb21
Nazmul Alam <nazmuldipu@gmail.com>
2018-09-01 18:24:46 +0600

    nav compnents added

---
+++ src/home/containers/order/order.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.scss']
+})
+export class OrderComponent{
+}
---
+++ src/home/containers/packages/packages.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'packages',
+  templateUrl: './packages.component.html',
+  styleUrls: ['./packages.component.scss']
+})
+export class PackagesComponent{}
