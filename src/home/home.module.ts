06cd39fef3a5dcf9c97dc453acc3c839263bda54
Nazmul Alam <nazmuldipu@gmail.com>
2018-09-01 17:33:02 +0600

    index page done

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -6,6 +6,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PlanComponent } from './components/plan/plan.component';
+import { ParallaxComponent } from './components/parallax/parallax.component';
+import { AdvantagesComponent } from './components/advantages/advantages.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BottomComponent } from './components/bottom/bottom.component';
 
 // routes
 export const ROUTES: Routes = [
@@ -17,6 +21,6 @@ export const ROUTES: Routes = [
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(ROUTES), NgbModule],
-  declarations: [IndexComponent, NavbarComponent, BannerComponent, PlanComponent]
+  declarations: [IndexComponent, NavbarComponent, BannerComponent, PlanComponent, ParallaxComponent, AdvantagesComponent, FooterComponent, BottomComponent]
 })
 export class HomeModule {}
